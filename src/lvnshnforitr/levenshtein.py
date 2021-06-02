cac865200dc6d7fb8e54af037c4903900faafb4f
yashinomi <by.yashinomi@gmail.com>
2021-06-02 15:29:27 +0900

    add docstring

--- src/lvnshnforitr/levenshtein.py
+++ src/lvnshnforitr/levenshtein.py
@@ -1,12 +1,34 @@
-from typing import Collection, List, TypeVar
+from typing import Callable, Collection, List, Optional, Tuple, TypeVar
 
 T = TypeVar("T")
 
 
 def distance(str1: Collection[T], str2: Collection[T], *,
              insert_cost: int = 1, delete_cost: int = 1, replace_cost: int = 1) -> int:
+    """
+    Calculates the Levenshtein distance between Collection objects T using dynamic programming.
+
+    Parameters
+    ----------
+    str1 : Collection[T]
+        Before
+    str2 : Collection[T]
+        After
+    insert_cost
+    delete_cost
+    replace_cost
+
+    Returns
+    -------
+    levenshtein_distance : int
+        Levenshtein distance between the Collections
+    """
+    # Caching the lengths
+    len_str1: int = len(str1)
+    len_str2: int = len(str2)
+    # Initialize table. Only dp_table[0, col_idx] and dp_table[row_idx, 0] matters
     dp_table: List[List[int]] = [
-        [col_idx + row_idx for col_idx in range(len(str1) + 1)] for row_idx in range(len(str2) + 1)
+        [col_idx + row_idx for col_idx in range(len_str1 + 1)] for row_idx in range(len_str2 + 1)
     ]
 
     for col_idx, item1 in enumerate(str1):
@@ -18,4 +40,4 @@ def distance(str1: Collection[T], str2: Collection[T], *,
                 dp_table[row_idx][col_idx] + replace_cost * int(is_replaced)
             )
 
-    return dp_table[row_idx + 1][col_idx + 1]
+    return dp_table[len_str2 + 1][len_str1 + 1]
