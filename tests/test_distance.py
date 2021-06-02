af1bc5fa6d09e5a1bc1a267b28f6aa9b1d098951
yashinomi <by.yashinomi@gmail.com>
2021-06-02 14:57:37 +0900

    add test for distance

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -19,6 +19,21 @@ class TestDistance(unittest.TestCase):
                 f"Error in case of {cor_str=}, {wrg_str=}, {dist=}"
             )
 
+    def test_collection_distance(self):
+        cor_str: List[str] = list('Levenshtein')
+        test_cases: List[Tuple[List[str], List[str], int]] = [
+            (cor_str, list('Lenvinsten'), 4),
+            (cor_str, list('Levensthein'), 2),
+            (cor_str, list('Levenshten'), 1),
+            (cor_str, list('Levenshtein'), 0)
+        ]
+
+        for cor_str, wrg_str, dist in test_cases:
+            self.assertEqual(
+                levenshtein.distance(cor_str, wrg_str), dist,
+                f"Error in case of {cor_str=}, {wrg_str=}, {dist=}"
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
