295aad4ef9b8513dfbf06feeeff109298fdd4969
yashinomi <by.yashinomi@gmail.com>
2021-06-03 16:35:57 +0900

    rename arguments

--- src/lvnshnforitr/levenshtein.py
+++ src/lvnshnforitr/levenshtein.py
@@ -4,15 +4,15 @@ from typing import Callable, Collection, List, Optional, Sequence, Tuple, TypeVa
 T = TypeVar("T")
 
 
-def align(str1: Sequence[T], str2: Sequence[T], *,
+def align(seq1: Sequence[T], seq2: Sequence[T], *,
           insert_cost: int = 1, delete_cost: int = 1, replace_cost: int = 1) \
         -> List[Tuple[Optional[T], Optional[T]]]:
     """
 
     Parameters
     ----------
-    str1
-    str2
+    seq1
+    seq2
     insert_cost
     delete_cost
     replace_cost
@@ -24,8 +24,8 @@ def align(str1: Sequence[T], str2: Sequence[T], *,
         When there is multiple ways to align, only one candidate is returned.
     """
     # Caching the lengths
-    len_str1: int = len(str1)
-    len_str2: int = len(str2)
+    len_str1: int = len(seq1)
+    len_str2: int = len(seq2)
     # Initialize table. Only dp_table[0, col_idx] and dp_table[row_idx, 0] matters
     dp_table: List[List[int]] = [
         [col_idx * delete_cost + row_idx * insert_cost for col_idx in range(len_str1 + 1)] for row_idx in
@@ -36,8 +36,8 @@ def align(str1: Sequence[T], str2: Sequence[T], *,
         [(-1, -1) for _1 in range(len_str1 + 1)] for _2 in range(len_str2 + 1)
     ]
 
-    for col_idx, item1 in enumerate(str1):
-        for row_idx, item2 in enumerate(str2):
+    for col_idx, item1 in enumerate(seq1):
+        for row_idx, item2 in enumerate(seq2):
             is_replaced = item1 != item2
             cost_if_inserted: int = dp_table[row_idx][col_idx + 1] + insert_cost
             cost_if_deleted: int = dp_table[row_idx + 1][col_idx] + delete_cost
@@ -64,19 +64,19 @@ def align(str1: Sequence[T], str2: Sequence[T], *,
         previous_idx = dp_index_memo[row_idx][col_idx]
         print(previous_idx)
         if previous_idx[0] + 1 == row_idx and previous_idx[1] == col_idx:
-            align_l_append((None, str2[row_idx - 1]))
+            align_l_append((None, seq2[row_idx - 1]))
         elif previous_idx[0] == row_idx and previous_idx[1] + 1 == col_idx:
-            align_l_append((str1[col_idx - 1], None))
+            align_l_append((seq1[col_idx - 1], None))
         else:
-            align_l_append((str1[col_idx - 1], str2[row_idx - 1]))
+            align_l_append((seq1[col_idx - 1], seq2[row_idx - 1]))
         row_idx, col_idx = previous_idx
 
     alignments_list: List[Tuple[Optional[T], Optional[T]]] = list(alignments_q)
 
     if row_idx > 0:
-        alignments_list = [(None, item2) for item2 in str2[:row_idx]] + alignments_list
+        alignments_list = [(None, item2) for item2 in seq2[:row_idx]] + alignments_list
     elif col_idx > 0:
-        alignments_list = [(item1, None) for item1 in str1[:col_idx]] + alignments_list
+        alignments_list = [(item1, None) for item1 in seq1[:col_idx]] + alignments_list
 
     return alignments_list
 
